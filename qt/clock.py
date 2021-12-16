61010e8a465823fd3f6fa2dc71db356983dd27ee
Puttipong <puttipong.ru@ku.th>
2021-12-16 21:47:47 +0700

    feat clock.py delete init

--- qt/clock.py
+++ qt/clock.py
@@ -6,42 +6,6 @@ from PyQt5.QtCore import QTimer, QTime, Qt
   
 class Window(QWidget):
   
-    def __init__(self):
-        super().__init__()
-  
-        # setting geometry of main window
-        self.setGeometry(100, 100, 800, 400)
-  
-        # creating a vertical layout
-        layout = QVBoxLayout()
-  
-        # creating font object
-        font = QFont('Arial', 120, QFont.Bold)
-  
-        # creating a label object
-        self.label = QLabel()
-  
-        # setting centre alignment to the label
-        self.label.setAlignment(Qt.AlignCenter)
-  
-        # setting font to the label
-        self.label.setFont(font)
-  
-        # adding label to the layout
-        layout.addWidget(self.label)
-  
-        # setting the layout to main window
-        self.setLayout(layout)
-  
-        # creating a timer object
-        timer = QTimer(self)
-  
-        # adding action to timer
-        timer.timeout.connect(self.showTime)
-  
-        # update the timer every second
-        timer.start(1000)
-  
     # method called by timer
     def showTime(self):
   
